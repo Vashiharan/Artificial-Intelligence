bb574c529cdd69c6ec8b77aa79943992e894be64
Vashiharan <vkoneswar@gmail.com>
2024-06-30 13:49:34 +0530

    crated 3_Gray-Image.py

--- 3_Gray-Image.py
+++ 3_Gray-Image.py
@@ -1,6 +1,6 @@
 # converting to gray image
 import cv2
-image = cv2.imread("novitech.png")
+image = cv2.imread("img.png")
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('original',image)
